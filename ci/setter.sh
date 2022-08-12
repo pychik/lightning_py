373ed22493643c3be65d066f0a4d380a7eba7f37
pychik <gendalf.grey2014@yandex.ru>
2022-08-12 03:42:10 +0300

    fixes prepared + timeouts api added

--- ci/setter.sh
+++ ci/setter.sh
@@ -7,10 +7,8 @@ cd /home/runner/work/lightning_py/lightning_py
 curl -O https://raw.githubusercontent.com/aerokube/selenium-openapi/master/selenium.yaml
 
 # install swagger codegen
-#git clone https://github.com/swagger-api/swagger-codegen
 wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.34/swagger-codegen-cli-3.0.34.jar -O swagger-codegen-cli.jar
 
-#mvn clean package
 java -jar swagger-codegen-cli.jar generate \
    -i /home/runner/work/lightning_py/lightning_py/selenium.yaml \
    -l python \
@@ -21,5 +19,6 @@ pushd python/ && python setup.py install && cd ..
 
 # checking code with flake8
 flake8 client/ --count --select=E9,F63,F7,F82,E302,W391,W292 --show-source --statistics
+
 # testing code
 pytest tests/ -W ignore::DeprecationWarning
